7a29ab6b86a92491962d57504ed482264bcf745a
Jahon <jokhuzb@mail.ru>
2024-11-26 13:28:50 +0500

    some updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
 import "./App.css";
 import RootLayout from "./layouts/RootLayout";
+import About from "./sections/About";
+import Head from "./sections/Head";
 
 function App() {
-  return <RootLayout></RootLayout>;
+  const [currentPage, setCurrentPage] = useState("home");
+  const pages = {
+    home: <Head />,
+    about: <About />,
+  };
+  return <RootLayout>{pages[currentPage]}</RootLayout>;
 }
 
 export default App;
