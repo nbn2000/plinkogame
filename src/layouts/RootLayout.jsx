b941fb60ab04f7b93e3f8c6c349dc1c8e397725b
nbn2000 <makhmudov4744@list.ru>
2024-11-26 17:07:40 +0500

    meta data added

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -12,9 +12,9 @@ const RootLayout = ({ children }) => {
       <I18nextProvider i18n={i18n}>
         <TopButton />
         <Navbar />
-        <div className="bg-[#f1f3f4] mt-[80px] p-14 my-10">
+        <div className="bg-[#f1f3f4] mt-[80px] p-4 md:p-14 my-10">
           <div className="container mx-auto ">
-            <h1 className="max-w-[1000px] text-[50px] font-roboto font-bold  leading-[55px]  ">
+            <h1 className="max-w-[1000px] text-[28px] md:text-[50px] font-roboto font-bold  md:leading-[55px]  ">
               Plinko Game: The Ultimate Guide to Online Gambling Fun
             </h1>
           </div>
